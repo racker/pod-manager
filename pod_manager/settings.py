9327b08201e88fc05c97f08515c6c2314600b05d
Tomaz Muraus <tomaz@tomaz.me>
2012-05-02 16:19:43 -0700

    Add settings.

--- pod_manager/settings.py
+++ pod_manager/settings.py
@@ -1,7 +1,13 @@
+from libcloud.compute.providers import Provider
+
 # Redis
 REDIS_HOST = '127.0.0.1'
 REDIS_PORT = 6379
 REDIS_DB = 0
+
+# Provider settings
+PROVIDER = Provider.RACKSPACE
+PROVIDER_CREDENTIALS = ('username', 'password')
 
 # Logging
 LOG_FORMAT = '%(asctime)s %(levelname)s (%(ip)s - %(session_id)s) - %(message)s'
