571b39ce7cf09617273d493e33ce7f2119fa4cb7
Tomaz Muraus <tomaz@tomaz.me>
2012-05-02 20:20:47 -0700

    Update default settings.

--- pod_manager/settings.py
+++ pod_manager/settings.py
@@ -1,3 +1,5 @@
+import logging
+
 from libcloud.compute.providers import Provider
 
 # Redis
@@ -8,9 +10,11 @@ REDIS_DB = 0
 # Provider settings
 PROVIDER = Provider.RACKSPACE
 PROVIDER_CREDENTIALS = ('username', 'password')
+PROVIDER_KWARGS = {}
 
 # Logging
-LOG_FORMAT = '%(asctime)s %(levelname)s (%(ip)s - %(session_id)s) - %(message)s'
+LOG_LEVEL = logging.DEBUG
+LOG_FORMAT = '%(asctime)s %(levelname)s - %(message)s'
 
 try:
     from local_settings import *
