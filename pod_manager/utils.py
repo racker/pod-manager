8732b76c56b25d77e7972706f3a335acf3986f14
Tomaz Muraus <tomaz@tomaz.me>
2012-05-02 20:20:59 -0700

    Modify get_logger to set level and formatter, add get_driver method.

--- pod_manager/utils.py
+++ pod_manager/utils.py
@@ -1,10 +1,27 @@
+import sys
 import logging
 
+from libcloud.compute.types import Provider
+from libcloud.compute.providers import get_driver as get_libcloud_driver
+
+from pod_manager.settings import LOG_LEVEL, LOG_FORMAT
+from pod_manager.settings import PROVIDER, PROVIDER_CREDENTIALS, PROVIDER_KWARGS
+
 __all__ = [
-    'get_logger'
+    'get_logger',
+    'get_driver'
 ]
 
 def get_logger(name):
     logger = logging.getLogger(name)
-    # TODO: set level, add handler
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler(sys.__stdout__)
+    formatter = logging.Formatter(LOG_FORMAT)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
     return logger
+
+def get_driver():
+    cls = get_libcloud_driver(PROVIDER)
+    driver = cls(*PROVIDER_CREDENTIALS, **PROVIDER_KWARGS)
+    return driver
